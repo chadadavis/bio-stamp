5d402b45a147dbb9b0ef3f6db660793eda489914
chad.a.davis <chad.a.davis@c50a4a15-7093-6cd1-5d9d-b53f2c045a12>
2011-05-04 17:22:21 +0000

    Removed boilerplate header, including outdated contact addresses and outdated license. This is now in the README.txt

--- trunk/src/ver2hor.c
+++ trunk/src/ver2hor.c
@@ -1,37 +1,3 @@
-/******************************************************************************
- The computer software and associated documentation called STAMP hereinafter
- referred to as the WORK which is more particularly identified and described in 
- the LICENSE.  Conditions and restrictions for use of
- this package are also in the LICENSE.
-
- The WORK is only available to licensed institutions.
-
- The WORK was developed by: 
-	Robert B. Russell and Geoffrey J. Barton
-
- Of current addresses:
-
- Robert B. Russell (RBR)	            Prof. Geoffrey J. Barton (GJB)
- EMBL Heidelberg                            School of Life Sciences
- Meyerhofstrasse 1                          University of Dundee
- D-69117 Heidelberg                         Dow Street
- Germany                                    Dundee, DD1 5EH
-                                          
- Tel: +49 6221 387 473                      Tel: +44 1382 345860
- FAX: +44 6221 387 517                      FAX: +44 1382 345764
- E-mail: russell@embl-heidelberg.de         E-mail geoff@compbio.dundee.ac.uk
- WWW: http://www.russell.emb-heidelberg.de  WWW: http://www.compbio.dundee.ac.uk
-
-   The WORK is Copyright (1997,1998,1999) Robert B. Russell & Geoffrey J. Barton
-	
-	
-	
-
- All use of the WORK must cite: 
- R.B. Russell and G.J. Barton, "Multiple Protein Sequence Alignment From Tertiary
-  Structure Comparison: Assignment of Global and Residue Confidence Levels",
-  PROTEINS: Structure, Function, and Genetics, 14:309--323 (1992).
-*****************************************************************************/
 #include "ver2hor.h"
 
 /* Reads in a STAMP file, and an optional parameter file and
