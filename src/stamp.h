f759ef2644e829bdfcf1484714303399468cb040
chad.a.davis <chad.a.davis@c50a4a15-7093-6cd1-5d9d-b53f2c045a12>
2011-09-23 13:44:53 +0000

    Increased id[4096] field size. Need a constant for this, generally

--- src/stamp.h
+++ src/stamp.h
@@ -17,7 +17,7 @@ struct brookn { /* structure to represent brookhaven residue numbers */
 
 struct domain_loc{		/* This structure allows rather complex domains to be described */
    char filename[4096];
-   char id[100];
+   char id[4096];
    int nobj;			/* The number of objects considered within the named file */
    int *type;			/* The type that each object is:
 					0 ==> an error
